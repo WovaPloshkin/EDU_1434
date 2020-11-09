34bc8e66981820a0d67052b8d7309d83e88d798e
WovaPloshkin <74148820+WovaPloshkin@users.noreply.github.com>
2020-11-09 10:15:23 +0300

    Create Main.java

---
+++ tasks/task1/task1.3/Main.java
@@ -0,0 +1,11 @@
+/* Объяви переменные a, b типа int и переменную s типа String.
+Сразу же в строке объявления присвой им любые разные значения. */
+
+public class Main {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        int a = 256652453;
+        int b = 675420987;
+        String s = "stringstringstring";
+    }
+}
