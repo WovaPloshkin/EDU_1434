df093fef4e5e455552baad25eff692e8e6286633
WovaPloshkin <74148820+WovaPloshkin@users.noreply.github.com>
2020-11-11 11:05:12 +0300

    Update Main.java

--- tasks/task1/task1.6/Main.java
+++ tasks/task1/task1.6/Main.java
@@ -1,9 +1,16 @@
 /*
 Пользователь вводит число, необходимо увеличить это число на 15% и вывести результат на экран
 */
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Ваш код
+
+    System.out.print("Введите число:");
+        Scanner scan = new Scanner(System.in);
+        double temp = scan.nextInt();
+        System.out.println(temp*1.15);
+
     }
 }
+
