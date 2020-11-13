3ea6ac4fdbf511c032dfa2aa031d7555fcc6005c
WovaPloshkin <74148820+WovaPloshkin@users.noreply.github.com>
2020-11-13 10:40:46 +0300

    Create Main.java

---
+++ tasks/task2/task2.2/Main.java
@@ -0,0 +1,31 @@
+/*Выведи на экран таблицу умножения 10 на 10 в следующем виде:
+1 2 3 4 ...
+2 4 6 8 ...
+3 6 9 12 ...
+4 8 12 16 ...
+... */
+package com.company;
+public class Main {
+    public static void main(String[] args) {
+       
+        for (int j = 1; j < 10; j++) {
+            for (int i = 0; i < 9; i++) {
+                System.out.print(j+i*j+ " ");
+
+            }
+            System.out.print("\n");
+        }
+
+    }
+}
+/*
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+}
+*/
