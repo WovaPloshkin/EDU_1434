1fa7283b1ea306fa303a00f0b9882807e11c92eb
WovaPloshkin <74148820+WovaPloshkin@users.noreply.github.com>
2020-11-16 11:35:18 +0300

    Update Main.java

--- tasks/task3/task3.1/Main.java
+++ tasks/task3/task3.1/Main.java
@@ -1,37 +1,45 @@
-/* Написать функцию, которая вычисляет минимум из трёх чисел.
+/*
+Написать функцию, которая вычисляет минимум из четырёх чисел.
+Функция min(a,b,c,d) должна использовать (вызывать) функцию min(a,b)
+Подсказка:
+Нужно написать тело двух существующих функций min.
 Требования:
 •	Программа должна выводить текст на экран.
-•	Метод min не должен выводить текст на экран.
-•	Метод main должен вызвать метод min четыре раза.
-•	Метод main должен выводить на экран результат работы метода min. Каждый раз с новой строки.
-•	Метод min должен возвращать минимальное значение из чисел a, b и с.
+•	Методы min не должны выводить текст на экран.
+•	Метод main должен вызывать метод min(a, b) и метод min(a, b, c, d).
+•	Метод main должен выводить на экран результат работы методов min. Каждый раз с новой строки.
+•	Метод min(a, b) должен возвращать минимальное значение из чисел a, b.
+•	Метод min(a, b, c, d) должен использовать метод min(a, b)
+•	Метод min(a, b, c, d) должен возвращать минимальное значение из чисел a, b, c, d.
 */
-
 package com.company;
 
 public class Main {
-    public static int min(int a, int b, int c) {
+
+
+    public static int min(int a, int b) {
         //напишите тут ваш код
-        int minvalue =0;
-        if(a<b) {
-            minvalue = a;
-        } else {
-            minvalue = b;
+        if (a>=b) return b;
+        else return a;
+    }
+private static int min(int aa,int bb,int cc,int dd) {
+        int min= 0;
+        min = min(aa,bb);
+        min = min(min,cc);
+        min = min(min,dd);
+        return min;
         }
 
-        if (minvalue >c) {
-            minvalue = c;
-        }
-    return minvalue;
+
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(min(-20, -10));
+        System.out.println(min(-40, -10, -30, 40));
+        System.out.println(min(-20, -40, -30, 40));
+        System.out.println(min(-20, -10, -40, 40));
+        System.out.println(min(-20, -10, -30, -40));
     }
-
-
-    public static void main(String[] args) {
-        System.out.println(min(1, 2, 3));
-
-        System.out.println(min(-1, -2, -3));
-        System.out.println(min(3, 5, 3));
-        System.out.println(min(5, 5, 10));
-    }
-
 }
+
+
+
