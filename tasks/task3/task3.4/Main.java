9f3da0d27f66677efefc579cfde985cc87ca10dc
WovaPloshkin <74148820+WovaPloshkin@users.noreply.github.com>
2020-11-16 16:02:21 +0300

    Create Main.java

---
+++ tasks/task3/task3.4/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        //напишите тут ваш ко
+
+        System.out.println(compare1(3 ,5));
+
+    }
+    public static boolean compare1(int a, int b) {
+     if (a == b) return true;
+     else return false;
+    }
+}
