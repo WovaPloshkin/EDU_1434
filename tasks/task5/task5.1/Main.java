3c9b5e4adad6d41070a381b7dff9bce2ef493d6c
WovaPloshkin <74148820+WovaPloshkin@users.noreply.github.com>
2020-11-20 12:28:52 +0300

    Create Main.java

---
+++ tasks/task5/task5.1/Main.java
@@ -0,0 +1,32 @@
+/*
+ * Дана коллекция имён.
+ * 1) удалить все повторяющиеся имена из коллекции
+ * 2) вывести коллекцию на экран
+ * */
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> names = new ArrayList();
+
+        names.add("Анатолий");
+        names.add("Георгий");
+        names.add("Руслан");
+        names.add("Георгий");
+        names.add("Павел");
+        names.add("Руслан");
+
+         List distinctElements = new ArrayList();
+         distinctElements=names.stream().distinct().collect(Collectors.toList());
+
+
+        System.out.println(names);
+        System.out.println(distinctElements);
+    }
+
+}
